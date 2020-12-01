38fb4acfb4a7dab5e7ccdb85de217db6ec00939c
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2020-12-01 17:40:16 +0900

    編集機能　修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  only: [:new, :create]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!,  except: [:index, :show]
+  #before_action :move_to_index, except: [:index, :show]
 
 
   
@@ -29,7 +29,15 @@ class ItemsController < ApplicationController
       #end
   end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   def show
     @item = Item.find(params[:id])
@@ -41,11 +49,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :describe, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :datetime_id, :price, :user).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
+  #def move_to_index
+    #unless user_signed_in?
+      #redirect_to new_user_session_path
+    #end
+  #end
   
 
 end
