90bad91501c925681dffa4134b622629f15786ca
seiichi621 <sei-0941-seiichi5@softbank.ne.jp>
2020-12-02 14:12:35 +0900

    コンフリクト解消2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  only: [:new, :create]
-  before_action :move_to_index, except: [:index, :show]
-
+  before_action :authenticate_user!,  except: [:index, :show]
+  
 
   
   def index
@@ -21,21 +20,38 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
+  
+  def edit
     @item = Item.find(params[:id])
     
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @item = Item.new
+    @item = Item.find(params[:id])
+    
+  end
+
+  #def destroy
+    #@item = Item.find(params[:id])
+    #@item.destroy
+    #redirect_to root_path
+  #end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :describe, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :datetime_id, :price, :user).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
   
 
